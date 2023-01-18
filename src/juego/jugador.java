ebfd2df2742613253a02a2db2a6854553818b74c
HellLonely <helllonely404@gmail.com>
2023-01-18 09:38:15 +0100

    Se añadio la reguex

--- src/juego/jugador.java
+++ src/juego/jugador.java
@@ -8,14 +8,21 @@ public class jugador {
     private byte numComodines;
     private String letra;
 
-    public jugador (String nombre){
-        this.nombre=nombre;
+    public jugador (String nombre, int dineroJugador){
+        this.nombre = nombre;
+        this.dineroJugador = dineroJugador;
     }
 
     public static void adivinarLetra(String letra){
         Scanner input = new Scanner(System.in);
-        letra=input.next("[a-z]{1}");
+        letra=input.next("[^aeiou]");
         System.out.print(letra);
+    }
+
+    public static void comprarVocal(String letra, int dineroJugador){
+        Scanner input = new Scanner(System.in);
+        letra=input.next("[^aeiou]");
+
     }
 
     public void setDineroJugador(int dineroJugador) {
